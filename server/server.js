f178e0c338a61a8b177e419696ace069498894ea
Alex Hill <big_al_hill@comcast.net>
2018-07-18 15:36:57 -0600

    server repo initial push

---
+++ server/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const massive = require("massive");
+const jwt = require('jsonwebtoken')
+const loginCtrl = require('./controllers/loginCtrl')
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+
+
+app.post('/register_user', loginCtrl.register)
+
+app.get('/login_user/:vb_username/:password', loginCtrl.login)
+app.get('/authenticateAuthToken/:token', loginCtrl.authenticateToken)
+
+
+
+
+app.listen('4000', console.log('ayyee listen fam on 4000'))
+
+
