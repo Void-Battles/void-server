7b6bb20841125495e4e79abac1196c5adee9a49f
Sam-Montoya <samuel.montoya1073@gmail.com>
2018-10-02 22:44:09 -0600

    added package json and invite fix

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const getAllUserInfo = async (_id, user) => {
   // Handle Invites
   let userInvites = await TEAM_INVITES.find({user_id: _id}, { team_id: true  })
   const filteredUserInvites = userInvites.map(team => team.team_id)
-  if(!filteredUserInvites) user.pending_invites = null
+  if(!filteredUserInvites) user.pending_invites = []
   const pendingTeams = await vb_teams.find( { _id: filteredUserInvites }, { team_pic: true, team_name: true, _id: true })
   user.pending_invites = userInvites.map(invite => {
     const team_info = pendingTeams.filter(team => team._id === invite.team_id)
